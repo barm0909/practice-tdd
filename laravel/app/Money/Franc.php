906056734c08ddfd51c65836032b8f4e8eef6c64
barm0909 <shinlikebaseball@gmail.com>
2021-12-09 21:55:10 +0900

    FrancでMoneyを継承してコンストラクタとequalsを削除した

--- laravel/app/Money/Franc.php
+++ laravel/app/Money/Franc.php
@@ -2,22 +2,10 @@
 
 namespace App\Money;
 
-class Franc
+class Franc extends Money
 {
-    private $amount;
-
-    public function __construct(int $amount)
-    {
-        $this->amount = $amount;
-    }
-
     public function times(int $multiplier)
     {
         return new Franc($this->amount * $multiplier);
-    }
-
-    public function equals(Franc $franc): bool
-    {
-        return $this->amount === $franc->amount;
     }
 }
