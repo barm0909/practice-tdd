7ee1cd7191454123ab3e766fbe277c886944d27d
barm0909 <shinlikebaseball@gmail.com>
2021-12-20 21:16:41 +0900

    Sumクラスを定義した

---
+++ laravel/app/Money/Sum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Money;
+
+class Sum
+{
+    private Money $augend;
+    private Money $addend;
+
+    // public function __construct(Money $augend, Money $addend)
+    // {
+    //     $this->augend = $augend;
+    //     $this->addend = $addend;
+    // }
+}
