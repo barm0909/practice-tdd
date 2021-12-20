66f0cc4b07ae0787bfd43bbb213b3d7b1e571bfa
barm0909 <shinlikebaseball@gmail.com>
2021-12-20 21:20:03 +0900

    plusメソッドでSumオブジェクトを返すようにした

--- laravel/app/Money/Sum.php
+++ laravel/app/Money/Sum.php
@@ -2,14 +2,14 @@
 
 namespace App\Money;
 
-class Sum
+class Sum implements Expression
 {
     private Money $augend;
     private Money $addend;
 
-    // public function __construct(Money $augend, Money $addend)
-    // {
-    //     $this->augend = $augend;
-    //     $this->addend = $addend;
-    // }
+    public function __construct(Money $augend, Money $addend)
+    {
+        // $this->augend = $augend;
+        // $this->addend = $addend;
+    }
 }
