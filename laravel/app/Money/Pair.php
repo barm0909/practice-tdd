163a1cc37a02e42d5cd38cb0ed47a204fc3bba1f
barm0909 <shinlikebaseball@gmail.com>
2021-12-23 21:34:39 +0900

    Pairクラスを定義した

---
+++ laravel/app/Money/Pair.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Money;
+
+class Pair
+{
+    private string $from;
+    private string $to;
+
+    public function __construct(string $from, string $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+}
