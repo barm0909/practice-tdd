37ef5713167dbc49c55fc852baa4f7028231d226
barm0909 <shinlikebaseball@gmail.com>
2021-12-24 21:27:06 +0900

    為替レートとBankクラスに格納するようにした

--- laravel/app/Money/Bank.php
+++ laravel/app/Money/Bank.php
@@ -4,6 +4,8 @@ namespace App\Money;
 
 class Bank
 {
+    private $rates = [];
+
     public function reduce(Expression $source, string $to): Money
     {
         return $source->reduce($this, $to);
