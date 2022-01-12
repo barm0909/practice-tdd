61f982d933247fdccd9fa7093ac31008c5de3cc6
barm0909 <shinlikebaseball@gmail.com>
2022-01-12 21:14:44 +0900

    was_runプロパティを定義した

--- laravel/public/Xunit/WasRun.php
+++ laravel/public/Xunit/WasRun.php
@@ -4,5 +4,10 @@ namespace Public\Xunit;
 
 class WasRun
 {
-    // pass
+    public $was_run;
+
+    public function __construct(bool $was_run)
+    {
+        $this->was_run = false;
+    }
 }
--- laravel/public/Xunit/Xunit.php
+++ laravel/public/Xunit/Xunit.php
@@ -5,6 +5,6 @@ namespace Public\Xunit;
 require_once 'WasRun.php';
 
 $test = new WasRun("testMethod");
-print($test->wasRun);
+print($test->was_run);
 $test->testMethod();
-print($test->wasRun);
+print($test->was_run);
