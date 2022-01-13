039c146cd924ff780b31699804076440ea3a3631
barm0909 <shinlikebaseball@gmail.com>
2022-01-13 21:29:32 +0900

    runメソッドを実装した

--- laravel/public/Xunit/WasRun.php
+++ laravel/public/Xunit/WasRun.php
@@ -2,22 +2,26 @@
 
 namespace Public\Xunit;
 
+use ReflectionMethod;
+
 class WasRun
 {
     public $was_run;
 
-    public function __construct(string | int $was_run)
+    public function __construct(string | int $was_run, $name)
     {
         $this->was_run = "NONE";
+        $this->name = $name;
+    }
+
+    public function run()
+    {
+        $method = $this->name;
+        (new ReflectionMethod($this))->invoke($method);
     }
 
     public function testMethod()
     {
         $this->was_run = 1;
-    }
-
-    public function run()
-    {
-        $this->testMethod();
     }
 }
