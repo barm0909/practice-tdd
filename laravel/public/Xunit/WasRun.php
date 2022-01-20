fc8701e3b93620837cacd6707b757dbd372d9204
barm0909 <shinlikebaseball@gmail.com>
2022-01-20 21:44:23 +0900

    was_runフラグを削除した

--- laravel/public/Xunit/WasRun.php
+++ laravel/public/Xunit/WasRun.php
@@ -6,19 +6,16 @@ require_once 'TestCase.php';
 
 class WasRun extends TestCase
 {
-    public $was_run;
     public $name;
     public $log;
 
     public function setUp()
     {
-        $this->was_run = "NONE";
         $this->log = "setUp ";
     }
 
     public function testMethod()
     {
-        $this->was_run = 1;
         $this->log = $this->log . "TestMethod ";
     }
 }
