00786bcd0cbff9516e14eb908729fe43bef5c066
barm0909 <shinlikebaseball@gmail.com>
2022-01-25 20:48:18 +0900

    コンストラクタを定義した

--- laravel/public/Xunit/TestResult.php
+++ laravel/public/Xunit/TestResult.php
@@ -4,8 +4,15 @@ namespace Public\Xunit;
 
 class TesrResult
 {
+    public $run_count;
+
+    public function __construct()
+    {
+        $this->run_count = 1;
+    }
+
     public function summary()
     {
-        return "1 run, 0 failed";
+        return "$this->run_count run, 0 failed";
     }
 }
